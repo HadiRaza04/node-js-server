0acd62e60fd83d22f2899e0c5dda90833a9da615
HadiRaza04 <hadiraza.edu@gmail.com>
2022-11-24 19:47:09 +0500

    first node-express-server

---
+++ server.mjs
@@ -0,0 +1,16 @@
+console.log("Hello Server");
+
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    console.log("request ip: ", req.ip);
+
+  res.send('Hello World! ' + new Date().toString());
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
